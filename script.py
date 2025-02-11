c61ae8817bcf94a827ba9cbcee202bc66cdb2f33
Arya <arya.t.innovate@gmail.com>
2025-02-11 17:34:33 +0330

    chore: define variable for current script path
    - This ensures the script
       doesn't move itself during
       the organization process.
    -  listdir: Lists all files and
       directories in the specified
       folder.

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
 import os
 import shutil
 
+def organize_files(directory):
+    # Get the name of this script (to avoid moving it)
+    script_name = os.path.basename(__file__)
+
+    for filename in os.listdir(directory):
+        # Getting full path of the file
+        file_path = os.path.join(directory, filename)
